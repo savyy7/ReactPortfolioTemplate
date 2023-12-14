8d5711be920b819594223dfb5cb5dc86506f2eee
Divy Saraswat <f20231113@hyderabad.bits-pilani.ac.in>
2023-12-14 17:08:27 +0530

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,9 +1,4 @@
 import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
